55a1342a8eb11823f27882b24f331ed8aebc55dc
rottik <michalrott@seznam.cz>
2017-01-03 16:42:15 +0100

    first commit

---
+++ DTW/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace DTW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("Cau");
+            set.Add("ahoj");
+            set.Add("dobry den");
+
+            string result = FindMostSimilar("ajoj", set);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Method find the most similar word from vocabulary for input word using Levenshtein distance.
+        /// </summary>
+        /// <param name="word">input word</param>
+        /// <param name="voc">vocabulary of words</param>
+        /// <returns></returns>
+        public static string FindMostSimilar(string word, HashSet<string> voc)
+        {
+            string result = "";
+            double minDist = double.MaxValue;
+
+            foreach (string refWord in voc)
+            {
+                double[,] A = new double[refWord.Length + 1, word.Length + 1];
+
+                A[0, 0] = 0;
+                for (int i = 1; i < refWord.Length+1; i++)
+                {
+                    A[i, 0] = double.MaxValue;
+                }
+                int j;
+                for (j = 1; j < word.Length+1; j++)
+                {
+                    A[0, j] = double.MaxValue;
+                }
+
+                int minI = 1;
+                j = 1;
+                while (j < word.Length)
+                {
+                    bool nahoru = false;
+                    int i = minI;
+                    while (i < refWord.Length )
+                    {
+                        double newWord = (i == 0 && j == 0) ? 0 : double.MaxValue;
+                        double over = 1*A[i - 1, j];
+                        double right = 1*A[i, j - 1];
+                        double rightOver = 1*A[i - 1, j - 1];
+
+                        //smer posunu ve slovo
+                        newWord = right;
+
+                        //smer posunu v referenci
+                        if (newWord > over) 
+                        { 
+                            newWord = over; 
+                            if (i == minI) 
+                                nahoru = true; 
+                        }
+
+                        //smer posunu v obou
+                        if (newWord > rightOver) 
+                            newWord = rightOver; 
+
+                        A[i, j] = (refWord[i-1] == word[j-1] ? 0 : 1) + newWord;
+                        i++;
+                    }
+                    if (nahoru) { minI++; }
+                    j++;
+                }
+                if (A[refWord.Length - 1, word.Length - 1] < minDist)
+                {
+                    minDist = A[refWord.Length - 1, word.Length - 1];
+                    result = refWord;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ DTW/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení jsou řízeny přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("DTW")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DTW")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení atributu ComVisible na hodnotu False udělá typy v tomto sestavení neviditelné 
+// pro komponenty modelu COM.  Jestliže potřebujete přistupovat k typům v tomto sestavení z 
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující identifikátor GUID slouží k identifikaci knihovny typů, jestliže je projekt vystaven objektům modelu COM
+[assembly: Guid("120c1c4b-e88b-4ce9-8efb-7f8ad740761a")]
+
+// Informace o verzi sestavení se skládá z následujících čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze 
+//      Číslo sestavení
+//      Revize
+//
+// Lze zadat všechny hodnoty nebo nechat hodnoty Čísel Sestavení a Revize ve výchozím nastavení 
+// použitím '*' jak je ukázáno dále:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
